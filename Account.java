066b0723b6bd816b36094697f871a9b38bfeabad
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-01-09 00:14:19 +0300

    Simple generation of account number and card pin done

--- Account.java
+++ Account.java
@@ -1,9 +1,37 @@
 package banking;
+import java.util.Random;
 
 public class Account {
-    private String cardNumber;
-    private String cardPin;
-    private long cardBalance;
+    private String cardNumber = "4000 00";
+    private String cardPin = "";
+    private long cardBalance = 0;
+
+    private void generateCardNumber() {
+        /**
+         * function to generate new account number.
+         * @return String number
+         */
+        Random random = new Random();
+        System.out.println("\nGeneration card number");
+        for (int i = 0; i < 10; i++) {
+            this.cardNumber += random.nextInt(10);
+        }
+        System.out.println(this.cardNumber + "\n");
+//        return cardNumber;
+    }
+
+    private void generatePinCode() {
+        /**
+         * function for generation Pin
+         */
+        Random random = new Random();
+        System.out.println("\nGeneration Pin Code");
+        for (int i = 0; i < 4; i++) {
+            this.cardPin += random.nextInt(10);
+        }
+        System.out.println(this.cardPin + "\n");
+//        return this.cardPin;
+    }
 
     public String getCardNumber() {
         return cardNumber;
@@ -11,5 +39,21 @@ public class Account {
 
     public void setCardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
+    }
+
+    public String getCardPin() {
+        return cardPin;
+    }
+
+    public void setCardPin(String cardPin) {
+        this.cardPin = cardPin;
+    }
+
+    public long getCardBalance() {
+        return cardBalance;
+    }
+
+    public void setCardBalance(long cardBalance) {
+        this.cardBalance = cardBalance;
     }
 }
