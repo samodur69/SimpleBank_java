96a28ce61795d29bda337f850d26da3105b92f1d
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-01-11 19:35:36 +0300

    change menu to switch-case

--- Main.java
+++ Main.java
@@ -6,40 +6,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         AccountManager manager = new AccountManager();
-        int choice;
         while (true) {
             MenuPrompt.printMenu(1);
-            choice = scan.nextInt();
-            if (choice == 1) {
-                manager.createAccount();
-            } else if (choice == 2) {
-                System.out.println("Enter your card number:");
-                String number = scan.next();
-                System.out.println("Enter your PIN:");
-                String pin = scan.next();
-                if (manager.logInAccount(number, pin)) {
-
-                    while (true) {
-                        MenuPrompt.printMenu(2);
-                        choice = scan.nextInt();
-                        if (choice == 1) {
-                            manager.showBalance();
-                        } else if (choice == 2) {
-                            manager.logOutAccount();
-                            break;
-                        } else if (choice == 0) {
-                            System.out.println("\nBye!");
-                            System.exit(0);
+            switch (scan.nextInt()) {
+                case 1:
+                    manager.createAccount();
+                    break;
+                case 2:
+                    System.out.println("Enter your card number:");
+                    String number = scan.next();
+                    System.out.println("Enter your PIN:");
+                    String pin = scan.next();
+                    if (manager.logInAccount(number, pin)) {
+                        while (true) {
+                            MenuPrompt.printMenu(2);
+                            switch (scan.nextInt()) {
+                            case 1:
+                                manager.showBalance();
+                                break;
+                            case 2:
+                                manager.logOutAccount();
+                                break;
+                            case 0:
+                                System.out.println("\nBye!");
+                                System.exit(0);
+                            default:
+                                System.out.println("Invalid choice. Try again!");
                         }
+                        }
+                    } else {
+                        System.out.println("Wrong card number or PIN!");
                     }
-                } else {
-                    System.out.println("Wrong card number or PIN!");
-                }
-            } else if (choice == 0) {
-                System.out.println("\nBye!");
-                System.exit(0);
-            } else {
-                System.out.println("Invalid choice. Try again!");
+                case 0:
+                    System.out.println("\nBye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Try again!");
             }
         }
     }
