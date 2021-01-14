00e559b174b5ccbb3e52efe68b84479930cefd88
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-01-14 22:56:18 +0300

    new login util. use data from DB

--- Accounts/AccountManager.java
+++ Accounts/AccountManager.java
@@ -5,9 +5,12 @@ import banking.data.DatabaseUtil;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ */
 public class AccountManager {
     private Account currentAccount;
-    public List<Account> cardAccounts = new ArrayList<>();
+//    public List<Account> sessionCardAccounts = new ArrayList<>();
     DatabaseUtil db = new DatabaseUtil(Main.getSavedArgs());
 
     /**
@@ -17,21 +20,20 @@ public class AccountManager {
 
         System.out.println("Your card has been created");
         Account acc = new Account();
-        this.cardAccounts.add(acc);
+//        this.sessionCardAccounts.add(acc);
         db.sqlAddNewAccount(acc);
     }
 
+    /**
+     * Login into account.
+     * check card/pin pair from DB. if no number in DB - return null
+     * @param inputNumber card number from user input
+     * @param inputPin card pin from user input
+     * @return true if number and pin correct
+     */
     public boolean logInAccount (String inputNumber, String inputPin){
-        for (Account acc: this.cardAccounts) {
-            if (inputNumber.equals(acc.getCardNumber())) {
-                if (inputPin.equals(acc.getCardPin())) {
-                    this.currentAccount = acc;
-                    System.out.println("You have successfully logged in!");
-                    return true;
-                }
-            }
-        }
-        return false;
+        this.currentAccount = db.importAccountInfo(inputNumber, inputPin);
+        return this.currentAccount != null;
     }
 
     public void logOutAccount() {
@@ -43,5 +45,4 @@ public class AccountManager {
     public void showBalance() {
         System.out.println("Balance: " + this.currentAccount.getCardBalance());
     }
-
 }
