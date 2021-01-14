690f48a59ed3e2e9cb44b238fb258eefd6e19ca6
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-01-14 20:59:11 +0300

    Create account Method done

--- data/DatabaseUtil.java
+++ data/DatabaseUtil.java
@@ -1,28 +1,78 @@
 package banking.data;
 
+import banking.Accounts.Account;
 import banking.FileUtil;
+import org.jetbrains.annotations.NotNull;
 import org.sqlite.SQLiteDataSource;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DatabaseUtil {
     private String url = "jdbc:sqlite:" + FileUtil.path;
-    SQLiteDataSource dataSource = new SQLiteDataSource();
+    final private String sqlUpdateAccount = "";
 
+    /**
+     * after initialization testing connection
+     * @param filename - adding DB filename to path from com line args to create full path to DB
+     */
     public DatabaseUtil(String filename) {
+        SQLiteDataSource dataSource = new SQLiteDataSource();
         this.url += filename;
         dataSource.setUrl(this.url);
         try (Connection con = dataSource.getConnection()) {
             if (con.isValid(5)) {
+                createTable();
                 System.out.println("Connection is valid");
             }
         } catch (SQLException e) {
-            System.out.println("Troubles with connection");
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * creating every time new table if not exists in DB
+     */
+    public void createTable() {
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(this.url);
+        try (Connection conn = dataSource.getConnection()) {
+            Statement state = conn.createStatement();
+            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS card (\n"
+                    + " id INTEGER AUTO_INCREMENT PRIMARY KEY,\n"
+                    + " number VARCHAR(16) NOT NULL,\n"
+                    + " pin VARCHAR(4) NOT NULL,\n"
+                    + " balance INTEGER DEFAULT '0' );";
+            state.executeUpdate(sqlCreateTable);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public Account importAccountInfo(String number, String pin) {
+        Account accInfo = new Account();
+        return accInfo;
+    }
+
+    /**
+     * func to add new row with already created account to DB
+     * @param account - object created in AccountManager
+     */
+    public void sqlAddNewAccount(@NotNull Account account) {
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(this.url);
+        try {
+            Connection conn = dataSource.getConnection();
+            String sqlNewAccount = "INSERT INTO card VALUES(?,?,?,?)";
+            PreparedStatement statement = conn.prepareStatement(sqlNewAccount);
+            statement.setInt(1, account.getUserId());
+            statement.setString(2, account.getCardNumber());
+            statement.setString(3, account.getCardPin());
+            statement.setInt(4, (int) account.getCardBalance());
+            statement.execute();
+        } catch (SQLException e) {
+            System.out.println("Troubles with insert new acc to DB");
+        }
+    }
 }
