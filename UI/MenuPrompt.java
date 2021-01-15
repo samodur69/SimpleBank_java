33f9df6d677f5407b3d902094de5a35773c6ad99
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-01-15 14:58:57 +0300

    small fixes

--- UI/MenuPrompt.java
+++ UI/MenuPrompt.java
@@ -2,7 +2,7 @@ package banking.UI;
 
 public class MenuPrompt {
     private final static String mainMenu = "\n1.Create an account\n2.Log into account\n0.Exit";
-    private final static String accountMenu = "\n1.Balance\n2.Log out\n0.Exit";
+    private final static String accountMenu = "\n1.Balance\n2.Add income\n3.Do transfer\n4.Close account\n5.Log out\n0.Exit";
 
     public static void printMenu(int menuCode) {
         switch (menuCode) {
